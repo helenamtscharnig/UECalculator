cb2be61d5dca7b2c5639484d16f90298b92cf08d
helenamtscharnig <s50824@edu.campus02.at>
2021-12-21 20:43:38 +0100

    Commit class Find Even inkl. Verständnisfragen

--- src/FindEven.java
+++ src/FindEven.java
@@ -7,5 +7,17 @@ public class FindEven {
             if (a%2 == 0){
             System.out.println(a);}
         }
+
+        for(int i = 1; i < 10; i++){
+            System.out.println(i);
+        }
+
+        for(int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }
+
+        for(int i = 20; i <= 90; i = i+5){
+            System.out.println(i);
+        }
     }
 }
