dc77b0bf9599efff689e10b9df8c56b3a30c8d22
helenamtscharnig <s50824@edu.campus02.at>
2021-12-21 17:55:49 +0100

    Commit Calculator Class with arithmetic operations

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,7 +10,48 @@ public class Calculator {
         System.out.println("The persons gender is " +gender+ " and the weight is " +weight+ " kg.");
         System.out.println("Is the person an adult? " +adult);
 
+        int a = 2;
+        int b = 4;
+
+        int addResult = add(a, b);
+        printResult(addResult);
+
+        int subResult = sub(a, b);
+        printResult(subResult);
+
+        int mulResult = mul(a, b);
+        printResult(mulResult);
+
+        int divResult = div(a, b);
+        printResult(divResult);
+
+        int modResult = mod(a, b);
+        printResult(modResult);
+
     }
 
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int sub(int a, int b){
+        return a - b;
+    }
+
+    public static int mul(int a, int b){
+        return a*b;
+    }
+
+    public static int div(int a, int b){
+        return a/b;
+    }
+
+    public static int mod(int a, int b){
+        return b%a;
+    }
+
+    public static void printResult(int result){
+        System.out.println("The result is: "+result);
+    }
 
 }
