6cd4eae7fc8545496e86353c191cb5c5b71988f1
helenamtscharnig <s50824@edu.campus02.at>
2021-12-21 16:51:18 +0100

    Initial commit: Calculator class

---
+++ .idea/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int age = 3;
+        double weight = 8.2;
+        boolean adult = false;
+        char gender = 'f';
+        String name = "Lisa";
+
+        System.out.println("The persons name is "+name+" and she is "+age+" years old.");
+        System.out.println("The persons gender is "+gender+" and the weight is "+weight+" kg.");
+        System.out.println("Is the person an adult? "+adult);
+
+    }
+}
