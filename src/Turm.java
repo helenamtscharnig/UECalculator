9330ea8ff5dd1953197615d2abafa136da11bce6
helenamtscharnig <s50824@edu.campus02.at>
2021-12-21 20:49:48 +0100

    Commit class Turm

---
+++ src/Turm.java
@@ -0,0 +1,13 @@
+public class Turm {
+
+    public static void main(String[] args) {
+        for(int a = 1; a <=10; a++){
+            System.out.println(a);
+        }
+
+        for(int b = 10; b >=1; b--) {
+            System.out.println(b);
+        }
+    }
+
+}
