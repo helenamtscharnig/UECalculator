6537ebf10c2999029f721d4be3c5e28baa0ec027
helenamtscharnig <s50824@edu.campus02.at>
2021-12-22 19:09:25 +0100

    Commit class ArrayDemo

---
+++ src/ArrayDemo.java
@@ -0,0 +1,20 @@
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        int[] arr = new int [3];
+        int[] arr2 = new int [3];
+
+        arr[0] = 10;
+        arr[1] = 2;
+        arr[2] = 4;
+
+        arr2[0] = 1;
+        arr2[1] = 3;
+        arr2[2] = 5;
+
+        System.out.println(arr[0]*arr2[0]);
+        System.out.println(arr[1]*arr2[1]);
+        System.out.println(arr[2]*arr2[2]);
+
+    }
+}
